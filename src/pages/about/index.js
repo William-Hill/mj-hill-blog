6f10d08a7ef51ff6a618249def3e2a0fd230c9e2
William Hill <mjh2225@gmail.com>
2019-07-24 16:23:52 -0400

    updated bio

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -22,18 +22,13 @@ const About = ({ data: { profilePhoto, skillIcons, toolIcons } }) => {
           <Img fluid={profilePhoto.childImageSharp.fluid} />
         </div>
         <div className={style.content}>
-          <h1>Hi, I'm Luigi!</h1>
-          <h2>Software Developer</h2>
+          <h1>Hi, I'm William!</h1>
+          <h2>Software Engineer, Teacher, and Photographer</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            cursus venenatis arcu, cursus pretium enim lacinia nec. Duis viverra
-            sagittis neque. Fusce non luctus urna. Vivamus suscipit metus ac
-            posuere egestas. Nunc a pulvinar purus. Vivamus nisi mi, fringilla
-            quis lacus et, sagittis mollis massa. Cras tempus massa quis
-            lobortis laoreet. Pellentesque metus odio, sagittis nec venenatis
-            non, maximus congue eros. Suspendisse pellentesque purus sit amet
-            ante commodo, et molestie mauris aliquet. Proin non nibh libero.
-            Fusce at nulla euismod, condimentum augue quis, convallis justo.
+            I am a full-stack engineer, writing tools for subject matter experts
+            in nonproliferation and climate science. I am also a proponent of
+            increasing diversity in the computing field by recruiting and
+            mentoring engineers from underrepresented groups.
           </p>
           <br />
           <h2>Skills</h2>
