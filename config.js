170db490001784849d7a684ffab0c50b7f79bbd0
William Hill <mjh2225@gmail.com>
2020-01-23 17:00:11 -0800

    updated config

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-
 module.exports = {
   pathPrefix: '/gatsby-starter-developer-blog',
-  siteUrl: 'https://lgcolella.github.io',
-  siteTitle: 'Luigi Colella',
-  siteDescription: 'Logbook of a software developer',
-  author: 'lcolella',
+  siteUrl: 'https://mjhilldigital.com',
+  siteTitle: 'M.J. Hill Digital',
+  siteDescription: 'Official website of M.J. Hill Digital, LLC',
+  author: 'M.J. Hill',
   postsForArchivePage: 3,
   defaultLanguage: 'en',
   disqusScript: 'https://luigi-colella.disqus.com/embed.js',
@@ -13,46 +12,51 @@ module.exports = {
     blog: 'blog',
     about: 'about',
     tag: 'tag',
-    archive: 'archive'
+    archive: 'archive',
+    speaking: 'speaking',
+    projects: 'projects',
+    media: 'media',
   },
   social: {
-    github: 'https://github.com/lgcolella',
-    linkedin: '',
-    rss: '/rss.xml'
+    github: 'https://github.com/William-Hill',
+    linkedin: 'https://www.linkedin.com/in/whill3',
+    instagram: 'https://www.instagram.com/emjay_hill/',
+    twitter: 'http://twitter.com/emjay_hill',
   },
   tags: {
     angular: {
-      description: 'Angular is an open source web application platform.'
+      description: 'Angular is an open source web application platform.',
     },
     electron: {
-      description: 'Electron is a framework for building cross-platform desktop applications with web technology.'
+      description:
+        'Electron is a framework for building cross-platform desktop applications with web technology.',
     },
     javascript: {
-      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.'
-    },
-    laravel: {
-      description: 'Laravel is a PHP framework for building web applications following the MVC pattern.'
+      description:
+        'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
     },
     nodejs: {
       name: 'Node.js',
-      description: 'Node.js is a tool for executing JavaScript in a variety of environments.'
+      description:
+        'Node.js is a tool for executing JavaScript in a variety of environments.',
     },
     rxjs: {
       name: 'RxJS',
-      description: 'RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.'
-    },
-    sass: {
-      description: 'Sass is a stable extension to classic CSS.'
+      description:
+        'RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.',
     },
     typescript: {
-      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.'
+      description:
+        'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
     },
     react: {
-      description: 'React is an open source JavaScript library used for designing user interfaces.'
+      description:
+        'React is an open source JavaScript library used for designing user interfaces.',
     },
     vuejs: {
       name: 'Vue.js',
-      description: 'Vue.js is a JavaScript framework for building interactive web applications.'
-    }
-  }
+      description:
+        'Vue.js is a JavaScript framework for building interactive web applications.',
+    },
+  },
 }
